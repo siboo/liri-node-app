7a2a92bd025d7a54cd98df1ecba6dbd52ef9afb2
Saba Z <sabaz@magtek.local>
2017-10-30 00:25:30 -0700

    Added the files

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: '<sBSciyrd1stOiCiG8tSkxLRtZ>',
+  consumer_secret: '<rDajpB4r739dV7oAkWGNtlPpIBfbyO2O9NGP3aNcmGxK8AEa2u>',
+  access_token_key: '<924895002898993152-qEOifHHIQXab5xl7286cHCSukXl8Fkg>',
+  access_token_secret: '<4X8Sy8MZkJ7y4anHONaEvvppE5XYUahJewwypdTxJ8xn7>',
+}
+
+module.exports = twitterKeys;
